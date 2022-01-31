1c20ac0a45afa7afbb3745e36f6c1116f2782dad
sisko81 <abdurcampbell@yahoo.com>
2022-01-31 18:30:26 +0900

    Created a function that randomly returns rock paper scissor in the console.

--- rps.js
+++ rps.js
@@ -1,4 +1,13 @@
-//Input data in prompt
+// Create a function called computer play,that will randomly return rock paper scissor.
 
-
-
+function computerPlay() {
+    let myArray = [
+        'rock',
+        'paper',
+        'scissor'
+]
+    let randomItems = myArray[Math.floor(Math.random() * myArray.length)];
+    return randomItems
+}
+let result = computerPlay()
+console.log(computerPlay(result))
