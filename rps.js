5b79617e64884a6c3f83861f852073bf66a63997
sisko81 <abdurcampbell@yahoo.com>
2022-02-02 00:55:02 +0900

    Update still working

--- rps.js
+++ rps.js
@@ -1,13 +1,68 @@
-// Create a function called computer play,that will randomly return rock paper scissor.
+// Create a function called computer play,that will randomly return rock paper scissor inside the console.
 
 function computerPlay() {
-    let myArray = [
+    const myArray = [
         'rock',
         'paper',
         'scissor'
 ]
-    let randomItems = myArray[Math.floor(Math.random() * myArray.length)];
+    const randomItems = myArray[Math.floor(Math.random() * myArray.length)];
     return randomItems
 }
-let result = computerPlay()
-console.log(computerPlay(result))
+const result = computerPlay()
+//console.log(computerPlay(result))
+
+
+//const choice = prompt('choose','')
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if(playerSelection ==='rock' )  {
+        console.log('I chose rock')
+    }
+  }
+  let myResults = playRound()
+  const playerSelection = 'rock';
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const choice = prompt('Choose: rock paper scissor','');
+if(choice == 'rock' || choice.toUpperCase()  ) {
+    console.log('you win')
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
