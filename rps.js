ee8022d1bd8d240deed6a9b0c031665150d66eac
sisko81 <abdurcampbell@yahoo.com>
2022-02-19 10:48:34 +0900

    Testing console output

--- rps.js
+++ rps.js
@@ -1,11 +1,18 @@
+//Pro_Tip: console.log many places to test the output
+
+console.log(computerPlay())
+
 // Create a function with the name computerPlay
 
 function computerPlay() {
-
+    
     //Inside this function declare two variables.
 
     let computerList = ['rock', 'paper', 'scissor'];
     let computerRandomList = computerList[Math.floor(Math.random() * 3)];
+
+    //Pro_Tip: console.log many places to test the output
+    console.log(computerRandomList);
 
     /*The first variable will be and array list, with the values of:
        rock, paper and scissor.
@@ -13,6 +20,10 @@ function computerPlay() {
        with the help of math. floor and math.random methods the length is 3 so multiply by 3*/
 
     // Return the computerRandomList, it will be used later on.
+
+    //Pro_Tip: console.log many places to test the output
+    console.log(computerRandomList);
+
     return computerRandomList;
 
 }
@@ -21,11 +32,8 @@ function computerPlay() {
 
 computerPlay()
 
-
-
-
-
-
+//Pro_Tip: use the console to make sure this is returning the expected output before moving to the next step!
+console.log(computerPlay())
 
 
 
