af4122a2023ba34fe531d1de2599d7117a90339d
sisko81 <abdurcampbell@yahoo.com>
2022-02-19 11:13:57 +0900

    update

--- rps.js
+++ rps.js
@@ -1,7 +1,3 @@
-//Pro_Tip: console.log many places to test the output
-
-console.log(computerPlay())
-
 // Create a function with the name computerPlay
 
 function computerPlay() {
@@ -11,18 +7,12 @@ function computerPlay() {
     let computerList = ['rock', 'paper', 'scissor'];
     let computerRandomList = computerList[Math.floor(Math.random() * 3)];
 
-    //Pro_Tip: console.log many places to test the output
-    console.log(computerRandomList);
-
     /*The first variable will be and array list, with the values of:
        rock, paper and scissor.
        The second variable will generate a random list 
        with the help of math. floor and math.random methods the length is 3 so multiply by 3*/
 
     // Return the computerRandomList, it will be used later on.
-
-    //Pro_Tip: console.log many places to test the output
-    console.log(computerRandomList);
 
     return computerRandomList;
 
@@ -33,6 +23,8 @@ function computerPlay() {
 computerPlay()
 
 //Pro_Tip: use the console to make sure this is returning the expected output before moving to the next step!
+//computerPlay function holds the value of computerRandomList inside the local scope.
+//The output will be random.
 console.log(computerPlay())
 
 
